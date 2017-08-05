da788554c895073475fa6435573aa2c3dc0c1bc5
pkelechi <pkelechi@seamfix.com>
2017-08-05 17:42:45 +0100

    encrypt password using bcrypt

--- server/server/controller/user.js
+++ server/server/controller/user.js
@@ -1,16 +1,22 @@
 //Import User model
 const User = require('../models').User;
+const bcrypt = require('bcrypt');
+const salt = bcrypt.genSaltSync(10);
+const dotenv = require('dotenv');
 
 module.exports = {
   signup(req, res) {
     let username = req.body.username;
     let email  = req.body.email;
     let password = req.body.password;
+    let hashedPassword = bcrypt.hashSync(password, salt);
+    //console.log(dotenv.load());
+    
     return User
       .create({
         username: username,
         email: email,
-        password: password,
+        password: hashedPassword,
         usertype: 'USER'
       })
       .then(user => res.status(201).send({
