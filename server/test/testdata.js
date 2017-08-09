0007ce215ab68697b1e21ba00b375d6935a511f6
pkelechi <pkelechi@seamfix.com>
2017-08-09 15:07:57 +0100

    Add hound and travis configuration

--- server/test/testdata.js
+++ server/test/testdata.js
@@ -1,7 +1,10 @@
+import randomstring from 'just.randomstring';
+
+
 //Test data for user signin and signup
-export const username = 'goodness';
-export const email = 'good@email.com';
-export const password = 'goodness';
+export const username = randomstring().substring(0, 8);
+export const email = username+'@'+'email.com';
+export const password = 'password';
 
 export const signinUsername = 'goodness';
 export const signinPassword = 'goodness';
@@ -15,8 +18,8 @@ export const bookIdFinished = 5;
 export const bookIdUnborrowed = 3;
 
 //Test data for book addition, edit and get functions
-export const bookName = 'Things fall apart';
-export const bookAuthor = 'Chinua Achebe';
+export const bookName = randomstring().substring(0, 8);
+export const bookAuthor = randomstring().substring(0, 8);
 export const bookDescription = 'A prose work of the 20th century';
 export const bookCategoryId = 2;
 export const bookQuantity = 2;
