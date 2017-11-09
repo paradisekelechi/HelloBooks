5ee32d99e6dd0e8b536647b47738cfb7a13c3e42
Paradise Kelechi <paradisekelechi@gmail.com>
2017-11-09 12:52:33 +0100

    chore(application): modify database configuration)
    modify database configuration file to use online database
    
    [delivers #152143646]

--- server/config/config.js
+++ server/config/config.js
@@ -7,11 +7,11 @@ export default {
     port: 5432,
     dialect: 'postgres'
   },
-  production: {
+  test: {
     use_env_variable: 'postgres://pcyausvn:Y7vc35xlYsNal2T2v1H2WspFoq8NZkUY@pellefant.db.elephantsql.com:5432/pcyausvn',
     dialect: 'postgres'
   },
-  test: {
+  production: {
     username: process.env.DB_USERNAME || 'andela',
     password: process.env.DB_PASSWORD || 'andela',
     database: process.env.DB_NAME || 'hellobooks-test',
