2c3a8a61de4d14800a96a22c81143bd3fda43203
Paradise Kelechi <paradisekelechi@gmail.com>
2017-12-05 09:34:41 +0100

    feature(application): modify email template
    [finishes #152161784]

--- test/server/accountTypeTest.js
+++ test/server/accountTypeTest.js
@@ -91,6 +91,25 @@ describe('Add Account type', () => {
         done();
       });
   });
+  it('should not be able to add a account type: level as not a number', (done) => {
+    request
+      .post(`${accountType}`)
+      .set('user-token', adminToken)
+      .send({
+        name: accountTypeName,
+        description: accountTypeDescription,
+        level: 'text'
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.equal(res.status, 400);
+        assert.equal(res.body.success, false);
+        assert.equal(res.body.message, 'Level should be a valid number');
+        done();
+      });
+  });
 });
 
 describe('Get Account types', () => {
