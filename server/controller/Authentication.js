2f1a85af6cb8e57c25b17f5dbee067a20ddff3a0
Paradise Kelechi <paradisekelechi@gmail.com>
2018-01-05 15:21:52 +0100

    feature(signin): add server-side implementation of google signup/signin
    - implement google signin at the server side
    - add tests to ensure functionality of google signin
    
    [starts #154086446]

--- server/controller/Authentication.js
+++ server/controller/Authentication.js
@@ -94,24 +94,16 @@ export default {
         }, secret, {
           expiresIn: 24 * 60 * 60 * 40
         });
-        if (user) {
-          res.status(200).send({
-            message: 'User Account Creation Successful',
-            token,
-            success: true,
-            email: user.dataValues.email,
-            userid: user.dataValues.id,
-            username: user.dataValues.username,
-            usertype: user.dataValues.user_type_id,
-            account_type: user.dataValues.account_type_id
-          });
-        } else {
-          res.status(401).send({
-            message: 'User account not created',
-            token,
-            success: true
-          });
-        }
+        res.status(200).send({
+          message: 'User Account Creation Successful',
+          token,
+          success: true,
+          email: user.dataValues.email,
+          userid: user.dataValues.id,
+          username: user.dataValues.username,
+          usertype: user.dataValues.user_type_id,
+          account_type: user.dataValues.account_type_id
+        });
       })
       .catch((error) => {
         if (error.name === 'SequelizeUniqueConstraintError') {
@@ -225,5 +217,139 @@ export default {
         message: 'Oops! User does not exist',
         success: false
       }));
-  }
+  },
+
+  googleSignin(req, res) {
+    let {
+      body: {
+        username,
+        email,
+        password
+      }
+    } = req;
+
+    if (validator.isEmpty(`${username}${empty}`) || username == null) {
+      res.status(401).send({
+        success: false,
+        message: 'Username is required'
+      });
+      return;
+    }
+    username = validator.trim(`${username}`);
+
+    if (validator.isEmpty(`${email}`) || email == null) {
+      res.status(401).send({
+        success: false,
+        message: 'Email is required'
+      });
+      return;
+    }
+    email = validator.trim(`${email}`);
+
+    if (validator.isEmpty(`${password}`) || password == null) {
+      res.status(401).send({
+        success: false,
+        message: 'Password is required'
+      });
+      return;
+    }
+    password = validator.trim(`${password}`);
+
+
+    if (!validator.isEmail(`${email}`)) {
+      res.status(401).send({
+        success: false,
+        message: 'Enter a valid email address'
+      });
+      return;
+    }
+
+    return User
+      .findOne({
+        where: {
+          email
+        }
+      })
+      .then((response) => {
+        if (!response) {
+          User
+            .findOne({
+              where: {
+                username
+              }
+            })
+            .then((usernameResponse) => {
+              if (!usernameResponse) {
+                User
+                  .create({
+                    username,
+                    email,
+                    password,
+                    active: true,
+                    deleted: false,
+                    use_count: 0,
+                    user_type_id: 1,
+                    account_type_id: 1
+                  })
+                  .then((userDetails) => {
+                    const token = jwt.sign({
+                      userid: userDetails.dataValues.id,
+                      email: userDetails.dataValues.email,
+                      username: userDetails.dataValues.username,
+                      usertype: userDetails.dataValues.user_type_id,
+                      account_type: userDetails.dataValues.account_type_id,
+                      image: userDetails.dataValues.image,
+                      useCount: userDetails.dataValues.use_count
+                    }, secret, {
+                      expiresIn: 24 * 60 * 60 * 40
+                    });
+                    res.status(200).send({
+                      message: 'User authenticated successfully',
+                      token,
+                      success: true,
+                      email: userDetails.dataValues.email,
+                      userid: userDetails.dataValues.id,
+                      username: userDetails.dataValues.username,
+                      usertype: userDetails.dataValues.user_type_id,
+                      account_type: userDetails.dataValues.account_type_id
+                    });
+                  });
+              } else {
+                res.status(400).send({
+                  success: false,
+                  message: 'User  username already exists'
+                });
+              }
+            });
+        } else {
+          const token = jwt.sign({
+            userid: response.dataValues.id,
+            email: response.dataValues.email,
+            username: response.dataValues.username,
+            usertype: response.dataValues.user_type_id,
+            account_type: response.dataValues.account_type_id,
+            image: response.dataValues.image,
+            useCount: response.dataValues.use_count
+          }, secret, {
+            expiresIn: 24 * 60 * 60 * 40
+          });
+          res.status(200).send({
+            message: 'User authenticated successfully',
+            token,
+            success: true,
+            email: response.dataValues.email,
+            userid: response.dataValues.id,
+            username: response.dataValues.username,
+            usertype: response.dataValues.user_type_id,
+            account_type: response.dataValues.account_type_id
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: 'no road',
+          error
+        });
+      });
+  },
 };
--- test/server/UserTest.js
+++ test/server/UserTest.js
@@ -13,6 +13,7 @@ const {
 } = chai;
 const {
   signin,
+  googleSignin,
   signup,
   getAllUsers,
   getAdminUsers,
@@ -157,6 +158,108 @@ describe('Signin Route', () => {
         assert.equal(res.body.success, false);
         assert.equal(res.body.message, 'Password is required');
         assert.equal(res.status, 401);
+      });
+    done();
+  });
+});
+
+describe('Google Signin Route', () => {
+  it('should be able to signin on initial usage', (done) => {
+    request
+      .post(googleSignin)
+      .send({
+        username: `google${username}`,
+        password: `google${password}`,
+        email: `google${email}`
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.exists(res.body.token);
+        assert.equal(res.status, 200);
+        assert.equal(res.body.success, true);
+        assert.equal(res.body.message, 'User authenticated successfully');
+        assert.equal(res.body.username, username);
+        assert.equal(res.body.email, email);
+        userToken = res.body.token;
+      });
+    done();
+  });
+
+  it('should be able to signin after having signed up previously', (done) => {
+    request
+      .post(googleSignin)
+      .send({
+        username: `google${username}`,
+        password: `google${password}`,
+        email: `google${email}`
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.exists(res.body.token);
+        assert.equal(res.status, 200);
+        assert.equal(res.body.success, true);
+        assert.equal(res.body.message, 'User authenticated successfully');
+        assert.equal(res.body.username, username);
+        assert.equal(res.body.email, email);
+        userToken = res.body.token;
+      });
+    done();
+  });
+
+  it('should not be able to signin using google: missing username', (done) => {
+    request
+      .post(signup)
+      .send({
+        password,
+        email
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.equal(res.status, 401);
+        assert.equal(res.body.success, false);
+        assert.equal(res.body.message, 'Username is required');
+      });
+    done();
+  });
+
+  it('should not be able to signin using google: missing password', (done) => {
+    request
+      .post(signup)
+      .send({
+        username,
+        email
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.equal(res.status, 401);
+        assert.equal(res.body.success, false);
+        assert.equal(res.body.message, 'Password is required');
+      });
+    done();
+  });
+
+  it('should not be able to signin using google: missing email', (done) => {
+    request
+      .post(signup)
+      .send({
+        username,
+        password
+      })
+      .end((err, res) => {
+        assert.exists(res.status);
+        assert.exists(res.body.success);
+        assert.exists(res.body.message);
+        assert.equal(res.status, 401);
+        assert.equal(res.body.success, false);
+        assert.equal(res.body.message, 'Email is required');
       });
     done();
   });
